a9e7e1e5650b3f0e1e1ca95d110117d956634d88
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2019-08-16 10:45:18 -0400

    soil

--- wp-content/plugins/soil/modules/google-analytics.php
+++ wp-content/plugins/soil/modules/google-analytics.php
@@ -6,15 +6,11 @@ namespace Roots\Soil\GoogleAnalytics;
  * Google Analytics snippet from HTML5 Boilerplate
  *
  * Cookie domain is 'auto' configured. See: http://goo.gl/VUCHKM
- * You can enable/disable this feature in functions.php (or app/setup.php if you're using Sage):
- * add_theme_support('soil-google-analytics', 'UA-XXXXX-Y', 'wp_footer', true);
- *
- * If you need to be GDPR compliant it is useful to set the last option to true,
- * this anonymizes IP addresses before sending data to Google.
+ * You can enable/disable this feature in functions.php (or lib/setup.php if you're using Sage):
+ * add_theme_support('soil-google-analytics', 'UA-XXXXX-Y', 'wp_footer');
  */
 function load_script() {
   $gaID = options('gaID');
-  $anomymizeIP = options('anonymizeIP');
   if (!$gaID) { return; }
   $loadGA = (!defined('WP_ENV') || \WP_ENV === 'production') && !current_user_can('manage_options');
   $loadGA = apply_filters('soil/loadGA', $loadGA);
@@ -26,11 +22,7 @@ function load_script() {
       (function(s,o,i,l){s.ga=function(){s.ga.q.push(arguments);if(o['log'])o.log(i+l.call(arguments))}
       s.ga.q=[];s.ga.l=+new Date;}(window,console,'Google Analytics: ',[].slice))
     <?php endif; ?>
-    ga('create','<?= $gaID; ?>','auto');
-    <?php if ($anomymizeIP) : ?>
-    ga('set', 'anonymizeIp', true);
-    <?php endif; ?>
-    ga('send','pageview')
+    ga('create','<?= $gaID; ?>','auto');ga('send','pageview')
   </script>
   <?php if ($loadGA) : ?>
     <script src="https://www.google-analytics.com/analytics.js" async defer></script>
@@ -44,7 +36,6 @@ function options($option = null) {
     $options = \Roots\Soil\Options::getByFile(__FILE__) + ['', 'wp_footer'];
     $options['gaID'] = &$options[0];
     $options['hook'] = &$options[1];
-    $options['anonymizeIP'] = &$options[2];
   }
   return is_null($option) ? $options : $options[$option];
 }
