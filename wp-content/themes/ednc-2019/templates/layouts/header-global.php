2c6428b5dd6f40d704406ce8529c14b8a7c63f34
Chris Johnson <chrisltd+cloud9@gmail.com>
2019-11-22 20:08:05 +0000

    finished mobile hamburger

--- wp-content/themes/ednc-2019/templates/layouts/header-global.php
+++ wp-content/themes/ednc-2019/templates/layouts/header-global.php
@@ -117,7 +117,32 @@ use Roots\Sage\Nav;
   </div>
   
   <div class="global-nav-mobile__footer">
-    Dolor sit
+    
+    <div class="global-nav-mobile__footer__social">
+           
+      <a class="icon-facebook" href="https://facebook.com/educationnc" target="_blank"></a>
+      <a class="icon-twitter" href="https://twitter.com/educationnc" target="_blank"></a>
+      <a class="icon-youtube" href="https://www.youtube.com/channel/UCJto5My-_AVw1Nx5AGq8TEQ" target="_blank"></a>
+      <a class="icon-instagram" href="https://instagram.com/educationnc" target="_blank"></a>
+      <a class="icon-rss" href="<?php echo get_bloginfo('rss2_url'); ?>"></a>
+     
+    </div>
+    
+    <div class="global-nav-mobile__footer__signup">
+    
+      <form action="https://ednc.us9.list-manage.com/subscribe/post?u=8ba11e9b3c5e00a64382db633&amp;id=2696365d99" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="your@email.com" required>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8ba11e9b3c5e00a64382db633_2696365d99" tabindex="-1" value=""></div>
+        <input type="submit" value="SUBMIT" name="subscribe" id="mc-embedded-subscribe" class="button">
+      </form>
+    
+    </div>
+    
+    <div class="global-nav-mobile__footer__txt-msg">
+      <a href="#">Sign up for alerts by text&nbsp;message</a>
+    </div>
+    
   </div>
 </div>
 
