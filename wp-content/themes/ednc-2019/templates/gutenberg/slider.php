bdbe68cec8124b46e39dc36e93d2c9b6b0af1a74
Chris Johnson <chrisltd+cloud9@gmail.com>
2019-12-05 21:13:49 +0000

    started slider output

--- wp-content/themes/ednc-2019/templates/gutenberg/slider.php
+++ wp-content/themes/ednc-2019/templates/gutenberg/slider.php
@@ -1,1 +1,20 @@
-<h1>I'm your amazing slider</h1>
+<?php if( have_rows('slides') ): ?>
+
+    <?php while ( have_rows('slides') ) : the_row(); ?>
+
+        <?php $image = get_sub_field('image'); ?>
+        
+        <?php if ($image): ?>
+        
+            <img src="<?php echo $image['sizes']['large'] ?>"><br>
+        
+        <?php endif; ?>
+
+        <b><?php the_sub_field('title'); ?></b>
+        <br>
+        <?php the_sub_field('caption'); ?>
+
+    <?php endwhile; ?>
+
+
+<?php endif; ?>
