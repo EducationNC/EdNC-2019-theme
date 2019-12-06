e762a30676418488d21e4cc7fa1a44b6d3d7c9aa
Chris Johnson <chrisltd+cloud9@gmail.com>
2019-12-06 19:41:17 +0000

    added slick slider

--- wp-content/themes/ednc-2019/templates/gutenberg/slider.php
+++ wp-content/themes/ednc-2019/templates/gutenberg/slider.php
@@ -1,20 +1,40 @@
+<?php $_id = rand(1000, 9999); ?>
+
 <?php if( have_rows('slides') ): ?>
 
-    <?php while ( have_rows('slides') ) : the_row(); ?>
+    <div class="slick-slider" id="slick-slider-<?php echo $_id ?>">
 
-        <?php $image = get_sub_field('image'); ?>
+        <?php while ( have_rows('slides') ) : the_row(); ?>
         
-        <?php if ($image): ?>
+            <div class="slick-slide">
+    
+                <?php $image = get_sub_field('image'); ?>
+                
+                <?php if ($image): ?>
+                
+                    <img src="<?php echo $image['sizes']['large'] ?>"><br>
+                
+                <?php endif; ?>
         
-            <img src="<?php echo $image['sizes']['large'] ?>"><br>
-        
-        <?php endif; ?>
-
-        <b><?php the_sub_field('title'); ?></b>
-        <br>
-        <?php the_sub_field('caption'); ?>
-
-    <?php endwhile; ?>
-
+                <b><?php the_sub_field('title'); ?></b>
+                <br>
+                <?php the_sub_field('caption'); ?>
+            
+            </div>
+    
+        <?php endwhile; ?>
+    
+    </div>
 
 <?php endif; ?>
+
+<script>
+window.addEventListener('load', function () {
+    var $ = jQuery;
+    
+    $(document).ready(function(){
+        $('#slick-slider-<?php echo $_id ?>').slick({});
+    });
+
+});
+</script>
