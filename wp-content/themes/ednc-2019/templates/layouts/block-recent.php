14554cd0c418e57000c1e36818cb89b9bc67c96f
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2019-12-10 13:50:25 -0500

    fixed subtitle on most recent

--- wp-content/themes/ednc-2019/templates/layouts/block-recent.php
+++ wp-content/themes/ednc-2019/templates/layouts/block-recent.php
@@ -6,7 +6,7 @@ $author_id = get_the_author_meta('ID');
 $author_bio = get_posts(array('post_type' => 'bio', 'meta_key' => 'user', 'meta_value' => $author_id));
 $featured_image = Media\get_featured_image('featured-four-block');
 
-/*
+
 $appearance = wp_get_post_terms(get_the_id(), 'appearance');
 if ($appearance) {
   $post_type = $column[0]->name;
@@ -21,33 +21,8 @@ elseif ($post->post_type == 'map') {
 else  {
   $post_type = "";
 }
-*/
 
-$cat_name = ''; // I have this set in some shortcodes
 
-if (!isset($cat_name) || $cat_name == '') {
-
-  if ( class_exists('WPSEO_Primary_Term') ) {
-
-    // Show the post's 'Primary' category, if this Yoast feature is available, & one is set. category can be replaced with custom terms
-
-    $wpseo_primary_term = new WPSEO_Primary_Term( 'category', get_the_id() );
-
-    $wpseo_primary_term = $wpseo_primary_term->get_primary_term();
-    $term               = get_term( $wpseo_primary_term );
-
-    if (is_wp_error($term)) {
-        $categories = get_the_terms(get_the_ID(), 'category');
-        $cat_name = $categories[0]->name;
-    } else {
-        $cat_name = $term->name;
-    }
-
-  } else {
-    $categories = get_the_terms(get_the_ID(), 'category');
-    $cat_name = $categories[0]->name;
-  }
-}
 
 ?>
 
@@ -57,7 +32,7 @@ if (!isset($cat_name) || $cat_name == '') {
       <!-- <img class="recent-block-image" src="<?php// echo $featured_image; ?>" /> -->
       <img src="<?php echo $featured_image; ?>" />
     </div>
-    <p class="caption"><?php echo $cat_name ?></p>
+    <p class="caption"><?php echo $post_type ?></p>
     <h3 class="post-title-recent"><?php the_title(); ?></h3>
     <?php get_template_part('templates/components/entry-meta-small'); ?>
     <a class="mega-link" href="<?php the_permalink(); ?>"></a>
