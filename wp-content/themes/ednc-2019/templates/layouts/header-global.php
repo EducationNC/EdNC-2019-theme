7b7f86f8f1510574332ba3965a9aaeaa7f612361
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2020-01-03 12:55:16 -0500

    header updates

--- wp-content/themes/ednc-2019/templates/layouts/header-global.php
+++ wp-content/themes/ednc-2019/templates/layouts/header-global.php
@@ -10,9 +10,9 @@ use Roots\Sage\Nav;
   </a>
 
   <div class="global-nav__container">
-    
+
     <div class="global-nav__row">
-  
+
       <div class="global-nav__row__left">
         <a href="#" class="global-nav__menu">
           <img src="<?php echo Assets\asset_path('images/header/menu-button.svg'); ?>" alt="Menu"
@@ -20,14 +20,14 @@ use Roots\Sage\Nav;
           <img src="<?php echo Assets\asset_path('images/header/close-button.svg'); ?>" alt="Close"
             class="global-nav__menu__icon--close">
         </a>
-        
+
         <?php $menus = get_nav_menu_locations(); ?>
         <?php if ($menu = wp_get_nav_menu_items($menus['left_navigation'])): foreach ($menu as $item): ?>
-          <a href="<?php echo $item->url ?>" class="global-nav__link <?php echo implode(" ", $item->classes) ?>"><?php echo $item->title ?></a>  
+          <a href="<?php echo $item->url ?>" class="global-nav__link <?php echo implode(" ", $item->classes) ?>"><?php echo $item->title ?></a>
         <?php endforeach; endif; ?>
-        
+
         <div class="global-nav__hamburger">
-          
+
          <?php
          wp_nav_menu(array(
            'theme_location' => 'primary_navigation',
@@ -35,84 +35,84 @@ use Roots\Sage\Nav;
             'depth' => 2
          ));
          ?>
-          
+
         </div>
-        
+
       </div>
-    
+
       <div class="global-nav__row__right">
-      
+
         <form action="/" method="get" class="global-nav__row__right__search">
           <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Search EdNC.org">
           <input type="image" alt="Search" src="<?php echo Assets\asset_path('images/header/search-icon.svg'); ?>">
         </form>
-        
+
         <?php $menus = get_nav_menu_locations(); ?>
         <?php if ($menu = wp_get_nav_menu_items($menus['right_navigation'])): foreach ($menu as $item): ?>
-          <a href="<?php echo $item->url ?>" class="global-nav__link <?php echo implode(" ", $item->classes) ?>"><?php echo $item->title ?></a>  
+          <a href="<?php echo $item->url ?>" class="global-nav__link <?php echo implode(" ", $item->classes) ?>"><?php echo $item->title ?></a>
         <?php endforeach; endif; ?>
-        
+
         <a href="#" class="global-nav__search">
-          <img src="<?php echo Assets\asset_path('images/header/search-button.svg'); ?>" alt="Search" 
+          <img src="<?php echo Assets\asset_path('images/header/search-button.svg'); ?>" alt="Search"
             class="global-nav__search__icon">
           <img src="<?php echo Assets\asset_path('images/header/close-button.svg'); ?>" alt="Close"
             class="global-nav__search__icon--close">
         </a>
-        
+
         <div class="global-nav__connect-menu">
-          
+
           <div class="global-nav__connect-menu__social">
-           
+
             <a class="icon-facebook" href="https://facebook.com/educationnc" target="_blank"></a>
             <a class="icon-twitter" href="https://twitter.com/educationnc" target="_blank"></a>
             <a class="icon-youtube" href="https://www.youtube.com/channel/UCJto5My-_AVw1Nx5AGq8TEQ" target="_blank"></a>
             <a class="icon-instagram" href="https://instagram.com/educationnc" target="_blank"></a>
             <a class="icon-rss" href="<?php echo get_bloginfo('rss2_url'); ?>"></a>
-           
+
           </div>
-          
+
           <div class="global-nav__connect-menu__signup">
-          
+
             <div class="global-nav__connect-menu__signup__title">Get Daily Headlines</div>
-          
+
             <form action="https://ednc.us9.list-manage.com/subscribe/post?u=8ba11e9b3c5e00a64382db633&amp;id=2696365d99" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
               <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="your@email.com" required>
               <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
               <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8ba11e9b3c5e00a64382db633_2696365d99" tabindex="-1" value=""></div>
               <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
             </form>
-            
+
           </div>
 
           <div class="global-nav__connect-menu__newsletters-link">
             <a href="/newsletters/">Get all newsletters</a>
           </div>
-          
+
           <div class="global-nav__connect-menu__txt-msg">
             <a href="#">Sign up for alerts by text&nbsp;message</a>
           </div>
-          
+
         </div>
-        
+
       </div>
-      
+
     </div>
-  
+
   </div>
-  
+
   <div class="global-nav__search-dropdown">
-  
+
     <form action="/" method="get" class="global-nav__search-dropdown__form">
       <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Search EdNC.org">
       <input type="image" alt="Search" src="<?php echo Assets\asset_path('images/header/search-icon.svg'); ?>">
     </form>
-        
+
   </div>
 
 </div>
 
 <div class="global-nav-mobile">
-  
+
   <div class="global-nav-mobile__main">
     <?php
     wp_nav_menu(array(
@@ -122,34 +122,46 @@ use Roots\Sage\Nav;
     ));
     ?>
   </div>
-  
+
   <div class="global-nav-mobile__footer">
-    
+
     <div class="global-nav-mobile__footer__social">
-           
+
       <a class="icon-facebook" href="https://facebook.com/educationnc" target="_blank"></a>
       <a class="icon-twitter" href="https://twitter.com/educationnc" target="_blank"></a>
       <a class="icon-youtube" href="https://www.youtube.com/channel/UCJto5My-_AVw1Nx5AGq8TEQ" target="_blank"></a>
       <a class="icon-instagram" href="https://instagram.com/educationnc" target="_blank"></a>
       <a class="icon-rss" href="<?php echo get_bloginfo('rss2_url'); ?>"></a>
-     
+
     </div>
-    
+
     <div class="global-nav-mobile__footer__signup">
-    
+
+
       <form action="https://ednc.us9.list-manage.com/subscribe/post?u=8ba11e9b3c5e00a64382db633&amp;id=2696365d99" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Get Daily Headlines" required>
+        <div class="mc-field-group input-group">
+          <ul>
+            <li><input type="checkbox" value="1" name="group[13145][1]" id="mce-group[13145]-13145-0" checked=checked><label for="mce-group[13145]-13145-0">Daily digest</label></li>
+            <li><input type="checkbox" value="2" name="group[13145][2]" id="mce-group[13145]-13145-1" checked=checked><label for="mce-group[13145]-13145-1">Weekly wrapup</label></li>
+            <li><input type="checkbox" value="64" name="group[13145][64]" id="mce-group[13145]-13145-2" checked=checked><label for="mce-group[13145]-13145-2">Breaking news alerts</label></li>
+            <li><input type="checkbox" value="2097152" name="group[13145][2097152]" id="mce-group[13145]-13145-3" checked=checked><label for="mce-group[13145]-13145-3">Reach roundup</label></li>
+            <li><input type="checkbox" value="4194304" name="group[13145][4194304]" id="mce-group[13145]-13145-4" checked=checked><label for="mce-group[13145]-13145-4">Friday@Five</label></li>
+            <li><input type="checkbox" value="8388608" name="group[13145][8388608]" id="mce-group[13145]-13145-5" checked=checked><label for="mce-group[13145]-13145-5">Awake 58</label></li>
+            <li><input type="checkbox" value="134217728" name="group[13145][134217728]" id="mce-group[13145]-13145-8" checked=checked><label for="mce-group[13145]-13145-8">Early Bird</label></li>
+          </ul>
+        </div>
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8ba11e9b3c5e00a64382db633_2696365d99" tabindex="-1" value=""></div>
         <input type="submit" value="SUBMIT" name="subscribe" id="mc-embedded-subscribe" class="button">
       </form>
-    
+
     </div>
-    
+
     <div class="global-nav-mobile__footer__txt-msg">
       <a href="#">Sign up for alerts by text&nbsp;message</a>
     </div>
-    
+
   </div>
 </div>
 
