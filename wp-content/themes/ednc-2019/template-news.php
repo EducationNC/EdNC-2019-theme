4f636888ae8732b8132f0241b8a103aca8dfe2c8
Chris Johnson <chrisltd+cloud9@gmail.com>
2020-01-12 21:07:18 +0000

    removed top image section from paginated news pages

--- wp-content/themes/ednc-2019/template-news.php
+++ wp-content/themes/ednc-2019/template-news.php
@@ -13,6 +13,7 @@ global $trending;
 $main = get_field('main-news-article');
 $posts = get_field('news-articles');
 $news_image = get_field('news-image');
+$firstPosts = array();
 
 ?>
 <section id="news" class="news search-results">
@@ -32,69 +33,71 @@ $news_image = get_field('news-image');
       </div>
     </div>
 
-    <?php
-    $args = array(
-      'posts_per_page' => 3,
-      'post_type' => array('post', 'map', 'ednews', 'edtalk', 'flash-cards'),
-      'paged' => get_query_var('paged'),
-      'tax_query' => array(
-        'relation' => 'AND',
-        array(
-          'taxonomy' => 'appearance',
-          'field'    => 'slug',
-          'terms'    => 'news',
+    <?php if (!(get_query_var('paged') &&  get_query_var('paged') > 1)): ?>
+    
+      <?php
+      $args = array(
+        'posts_per_page' => 3,
+        'post_type' => array('post', 'map', 'ednews', 'edtalk', 'flash-cards'),
+        'paged' => get_query_var('paged'),
+        'tax_query' => array(
+          'relation' => 'AND',
+          array(
+            'taxonomy' => 'appearance',
+            'field'    => 'slug',
+            'terms'    => 'news',
+          ),
+          array(
+            'taxonomy' => 'appearance',
+            'field' => 'slug',
+            'terms' => 'hide-from-archives',
+            'operator' => 'NOT IN'
+          )
         ),
-        array(
-          'taxonomy' => 'appearance',
-          'field' => 'slug',
-          'terms' => 'hide-from-archives',
-          'operator' => 'NOT IN'
-        )
-      ),
-      // 'orderby' => 'publish_date',
-      'order' => 'DESC'
-    );
-
-    $trending = new WP_Query($args);
-    ?>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="category-content">
-            <?php if ($trending->have_posts()) :
-
-              $firstPosts = array();
-
-              while ($trending->have_posts()) : $trending->the_post();
-
-                $firstPosts[] = $post->ID;
-                $featured_image = Media\get_featured_image('featured-four-block');
-                $size = 'full'; // (thumbnail, medium, large, full or custom size)?>
+        // 'orderby' => 'publish_date',
+        'order' => 'DESC'
+      );
   
-                <article <?php post_class('block-news content-block-3 clearfix'); ?>>
-                  <div class="flex">
-                    <div class="block-content">
-                      <?php if (!empty($featured_image)) { ?>
-                        <img class="" src="<?php echo $featured_image; ?>" />
-                      <?php } else { ?>
-                        <div class="circle-image">
-                          <?php echo $author_pic; ?>
-                        </div>
-                      <?php } ?>
-                      <h3 class="post-title"><?php the_title(); ?></h3>
-                      <?php get_template_part('templates/components/entry-meta'); ?>
-                      <a class="mega-link" href="<?php the_permalink(); ?>"></a>
-                      <p class="lato"><?php echo wp_trim_excerpt(); ?></p>
+      $trending = new WP_Query($args);
+      ?>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="category-content">
+              <?php if ($trending->have_posts()) :
+  
+                while ($trending->have_posts()) : $trending->the_post();
+  
+                  $firstPosts[] = $post->ID;
+                  $featured_image = Media\get_featured_image('featured-four-block');
+                  $size = 'full'; // (thumbnail, medium, large, full or custom size)?>
+    
+                  <article <?php post_class('block-news content-block-3 clearfix'); ?>>
+                    <div class="flex">
+                      <div class="block-content">
+                        <?php if (!empty($featured_image)) { ?>
+                          <img class="" src="<?php echo $featured_image; ?>" />
+                        <?php } else { ?>
+                          <div class="circle-image">
+                            <?php echo $author_pic; ?>
+                          </div>
+                        <?php } ?>
+                        <h3 class="post-title"><?php the_title(); ?></h3>
+                        <?php get_template_part('templates/components/entry-meta'); ?>
+                        <a class="mega-link" href="<?php the_permalink(); ?>"></a>
+                        <p class="lato"><?php echo wp_trim_excerpt(); ?></p>
+                      </div>
                     </div>
-                  </div>
-                </article>
-
-            <?php endwhile; endif; wp_reset_query(); ?>
-
+                  </article>
+  
+              <?php endwhile; endif; wp_reset_query(); ?>
+  
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    
+    <?php endif; ?>
 
     <?php
     $paged1 = get_query_var('paged') ? get_query_var('paged') : 1;
