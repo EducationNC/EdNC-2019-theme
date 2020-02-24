f5b7114149c0574dacfc812c7b4df56723ceb598
Chris Johnson <chrisltd+cloud9@gmail.com>
2020-02-24 18:23:24 +0000

    added annual report tests

--- tests/__tests__/posts.test.js
+++ tests/__tests__/posts.test.js
@@ -30,15 +30,39 @@ describe('Post: Voter’s Guide 2020', () => {
   })
 
   it('headline should display "Meet the candidates for state superintendent" on page', async () => {
-    await expect(page).toMatch('Meet the candidates for state superintendent')
-  })
+    await expect(page).toMatchElement('h1', {text: 'Voter’s Guide: Meet the candidates for state superintendent'})
+  }, 10000)
   
   it('should display "What grade would you give North Carolina public schools?" text on page', async () => {
     await expect(page).toMatch('What grade would you give North Carolina public schools?')
-  })
+  }, 10000)
   
   it('should display date "February 13, 2020"', async () => {
     await expect(page).toMatch('February 13, 2020')
+  }, 10000)
+  
+})
+
+
+describe('Post: Annual Report 2019-2020', () => {
+  beforeAll(async () => {
+    await page.goto(baseURL + '/annual-report-19-20/');
   })
   
+  it('should display "EducationNC’s Annual Report, 2019-20" text on page', async () => {
+    await expect(page).toMatch('EducationNC’s Annual Report, 2019-20')
+  }, 10000)
+  
+  it('should display "About EdNC" text on page', async () => {
+    await expect(page).toMatchElement('h1', {text: 'About EdNC'})
+  }, 10000)
+  
+  it('should display video on page', async () => {
+    await expect(page).toMatchElement('.videoWrapper iframe')
+  }, 10000)
+  
+  it('should display embed on page', async () => {
+    await expect(page).toMatchElement('.wp-block-embed-wordpress')
+  }, 10000)
+
 })
