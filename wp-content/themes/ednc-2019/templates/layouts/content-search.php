9a704e305f8c9b5e7fe4d2d00b6573a5105920c1
Chris Johnson <chrisltd+cloud9@gmail.com>
2020-03-11 18:36:55 +0000

    added img thumbnail fallback

--- wp-content/themes/ednc-2019/templates/layouts/content-search.php
+++ wp-content/themes/ednc-2019/templates/layouts/content-search.php
@@ -1,9 +1,16 @@
 <?php
 $img = $post->cse_img;
+if (!$img) {
+  $image_id = get_post_thumbnail_id();
+  $featured_image = wp_get_attachment_image_src($image_id, 'medium');
+  if ($featured_image) {
+    $img = $featured_image[0]; 
+  }
+}
 ?>
 <article <?php post_class('block-post clearfix'); ?>>
   <a class="mega-link" href="<?php the_permalink(); ?>"></a>
-  <?php if ($img) { ?>
+  <?php if ($img): ?>
     <div class="col-sm-5">
       <div class="photo-overlay">
         <img class="post-thumbnail" src="<?php echo $img; ?>" />
@@ -11,9 +18,9 @@ $img = $post->cse_img;
     </div>
 
     <div class="col-sm-7">
-  <?php } else { ?>
+  <?php else: ?>
     <div class="col-sm-12">
-  <?php } ?>
+  <?php endif; ?>
       <header>
         <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
         <?php if (get_post_type() === 'post') { get_template_part('templates/components/entry-meta'); } ?>
