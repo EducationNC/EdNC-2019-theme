f614d88e6cb1d195fd103cca02daddfd5a41bdcd
Chris Johnson <chrisltd+cloud9@gmail.com>
2020-03-11 19:21:39 +0000

    styled adv search

--- wp-content/themes/ednc-2019/page-advanced-search.php
+++ wp-content/themes/ednc-2019/page-advanced-search.php
@@ -5,6 +5,34 @@
     <div class="col-md-8 col-centered">
 
       <?php echo facetwp_display( 'facet', 'search' ) ?>
+     
+      <fieldset class="search__filters">
+      
+        <div class="row">
+        
+          <div class="col-md-12">
+            <label class="search__filters__label">Date</label>
+            <?php echo facetwp_display( 'facet', 'date' ) ?>
+          </div>
+        
+          <div class="col-md-4">
+            <label class="search__filters__label">Topic</label>
+            <?php echo facetwp_display( 'facet', 'category' ) ?>
+          </div>
+          
+          <div class="col-md-4">
+          <label class="search__filters__label">Category</label>
+            <?php echo facetwp_display( 'facet', 'type' ) ?>
+          </div>
+          
+          <div class="col-md-4">
+            <label class="search__filters__label">Author</label>
+            <?php echo facetwp_display( 'facet', 'author' ) ?>
+          </div>
+        
+        </div>
+        
+      </fieldset>
       
       <div class="row">
         <?php echo facetwp_display( 'template', 'results' ) ?>
