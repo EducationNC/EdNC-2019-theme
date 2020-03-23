a3e9bffcb8c0219e5d722d98eab8a68e44472123
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2020-03-23 10:24:36 -0400

    test header and footer

--- wp-content/mu-plugins/acf-fields-post-types/acf-fields-header-footer.php
+++ wp-content/mu-plugins/acf-fields-post-types/acf-fields-header-footer.php
@@ -424,7 +424,7 @@ acf_add_local_field_group(array(
 			array(
 				'param' => 'options_page',
 				'operator' => '==',
-				'value' => 'acf-options-footer',
+				'value' => 'acf-options-header-and-footer',
 			),
 		),
 	),
