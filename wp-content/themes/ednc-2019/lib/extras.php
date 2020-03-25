779cc16a4840eb43fd9e869b55cd827e697aafc7
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2020-03-25 19:57:56 -0400

    added search filters back in

--- wp-content/themes/ednc-2019/lib/extras.php
+++ wp-content/themes/ednc-2019/lib/extras.php
@@ -321,10 +321,9 @@ add_filter( 'facetwp_index_row', function( $params, $class ) {
 
 // Relevanssi exclude posts in hide from archives
 
-/*
+
 add_filter('relevanssi_do_not_index', 'rlv_exclude_cat', 10, 2);
 function rlv_exclude_cat($exclude, $post_id) {
     if (has_term('hide-from-archives', 'appearance', $post_id )) $exclude = true;
     return $exclude;
 }
-*/
